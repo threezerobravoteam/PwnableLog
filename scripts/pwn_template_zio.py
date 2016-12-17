c7f75536901445e3d8efe6bd6263893f956621e0
“o0xmuhe” <o0xmuhe@gmail.com>
2016-12-17 22:57:42 +0800

    exploit template using zio

---
+++ scripts/pwn_template_zio.py
@@ -0,0 +1,23 @@
+from zio import *
+
+
+LOCAL = True
+
+if LOCAL:
+	target = './filename'
+else:
+	target = ('127.0.0.1',10001)
+
+io = zio(target, timeout=10000, print_read=COLORED(RAW, 'red'), print_write=COLORED(RAW, 'green'))
+
+
+
+def main():
+	pass
+
+if __name__ =='__main__':
+	main()
+
+
+
+io.interact()
